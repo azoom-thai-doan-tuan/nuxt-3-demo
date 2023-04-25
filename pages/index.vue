cc88141083053fe2d949a0498a5eb085131a3055
Thai Doan Tuan <thai.doan.tuan@azoom.jp>
2023-04-25 15:39:44 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,68 @@
 <template>
   <div class="counter">
-    <form @submit.prevent="submitForm" id="ng-fm" class="ng-fm">
-      <div class="ng-fm-row">
-        <input
-          type="text"
-          v-model="formData.fullName"
-          class="ng-inp"
-          placeholder="Full Name"
-        />
-      </div>
-      <div class="ng-fm-row">
-        <input
-          type="text"
-          v-model="formData.email"
-          class="ng-inp"
-          placeholder="Email"
-        />
-      </div>
-      <div class="ng-fm-row">
-        <input
-          type="number"
-          v-model="formData.phoneNo"
-          class="ng-inp"
-          placeholder="Phone Number"
-        />
-      </div>
-      <div class="ng-fm-row">
-        <input
-          type="text"
-          v-model="formData.service"
-          class="ng-inp"
-          placeholder="Service"
-        />
-      </div>
-      <div class="ng-fm-row">
-        <textarea
-          name=""
-          v-model="formData.message"
-          class="ng-inp"
-          placeholder="Message"
-        ></textarea>
-      </div>
-      <div class="ng-fm-row">
-        <button class="ng-bt-pri" type="submit" @click="submit">
-          Send Message
-        </button>
-      </div>
-    </form>
+    <div class="ng-fm-row">
+      <p>Users: {{ result.users.toLocaleString() }}</p>
+    </div>
+    <div class="ng-fm-row">
+      <p>Number of plants: {{ result.numberOfPlants.toLocaleString() }}</p>
+    </div>
+    <div class="ng-fm-row">
+      <p>Legend plants: {{ result.legendPlants.toLocaleString() }}</p>
+    </div>
+    <div class="ng-fm-row">
+      <p>Epic plants: {{ result.epicPlants.toLocaleString() }}</p>
+    </div>
+    <div class="ng-fm-row">
+      <p>Rare plants: {{ result.rarePlants.toLocaleString() }}</p>
+    </div>
+    <div class="ng-fm-row">
+      <p>Common plants: {{ result.commonPlants.toLocaleString() }}</p>
+    </div>
+    <div class="ng-fm-row">
+      <p>Mother trees: {{ result.motherTrees.toLocaleString() }}</p>
+    </div>
+    <div class="ng-fm-row">
+      <p>Old plants: {{ result.oldPlants.toLocaleString() }}</p>
+    </div>
+    <div class="ng-fm-row">
+      <p>Old lands: {{ result.oldLands.toLocaleString() }}</p>
+    </div>
+    <div class="ng-fm-row">
+      <p>Free tickets: {{ result.numberOfFreeTicket.toLocaleString() }}</p>
+    </div>
+    <div class="ng-fm-row">
+      <p>
+        Ticket in current round:
+        {{ result.numberOfTicketInRound.toLocaleString() }}
+      </p>
+    </div>
+    <div class="ng-fm-row">
+      <p>Flipcoin Win: {{ result.numberOfBettingWin.toLocaleString() }}</p>
+    </div>
+    <div class="ng-fm-row">
+      <p>Flipcoin Lose: {{ result.numberOfBettingLose.toLocaleString() }}</p>
+    </div>
+    <div class="ng-fm-row">
+      <p>Flipcoin X10: {{ result.times10BettingCount.toLocaleString() }}</p>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { formValid } from "../schema/form";
+import { formValid } from '../schema/form'
+
+const { data } = await useFetch('https://api.plantvsundead.com/analytics')
+const result = data._value
 
 const formData = reactive({
-  fullName: "John",
-  email: "Doe",
-  message: "Hello",
-  phoneNo: "",
-  service: "",
-});
+  fullName: 'John',
+  email: 'Doe',
+  message: 'Hello',
+  phoneNo: '',
+  service: '',
+})
 function submit() {
-  const { success } = formValid.safeParse(formData);
-  console.log(success);
+  const { success } = formValid.safeParse(formData)
+  console.log(success)
 }
 </script>
