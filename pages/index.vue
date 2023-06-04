7c7870339e1c480854b1b22ad4539685054593f2
Thai Doan Tuan <thai.doan.tuan@azoom.jp>
2023-06-04 10:55:18 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,105 +1,115 @@
 <template>
-  <div class="counter">
-    <div class="ng-fm-row">
-      <p>Users: {{ result.users.toLocaleString() }}</p>
+  <div class="counter-page">
+    <div>
+      <div class="ng-fm-row">
+        <p>Users: {{ result.users.toLocaleString() }}</p>
+      </div>
+      <div class="ng-fm-row">
+        <p>Number of plants: {{ result.numberOfPlants.toLocaleString() }}</p>
+      </div>
+      <div class="ng-fm-row">
+        <p>Number of decorations: {{ result.decors.toLocaleString() }}</p>
+      </div>
+      <br />
+      <div class="ng-fm-row">
+        <p>Number of bidding slots: {{ result.bidding.toLocaleString() }}</p>
+      </div>
+      <div class="ng-fm-row">
+        <p>Number of ceiling bids: {{ result.highestBid.toLocaleString() }}</p>
+      </div>
+      <br />
+
+      <div class="ng-fm-row">
+        <p>Legend plants: {{ result.legendPlants.toLocaleString() }}</p>
+      </div>
+      <div class="ng-fm-row">
+        <p>Epic plants: {{ result.epicPlants.toLocaleString() }}</p>
+      </div>
+      <div class="ng-fm-row">
+        <p>Rare plants: {{ result.rarePlants.toLocaleString() }}</p>
+      </div>
+      <div class="ng-fm-row">
+        <p>Common plants: {{ result.commonPlants.toLocaleString() }}</p>
+      </div>
+      <div class="ng-fm-row">
+        <p>Mother trees: {{ result.motherTrees.toLocaleString() }}</p>
+      </div>
+      <div class="ng-fm-row">
+        <p>Fused plants: {{ result.fused.toLocaleString() }}</p>
+      </div>
+      <div class="ng-fm-row">
+        <p>Mythic 1 plants: {{ result.omega1.toLocaleString() }}</p>
+      </div>
+      <div class="ng-fm-row">
+        <p>Mythic 2 plants: {{ result.omega2.toLocaleString() }}</p>
+      </div>
+      <br />
     </div>
-    <div class="ng-fm-row">
-      <p>Number of plants: {{ result.numberOfPlants.toLocaleString() }}</p>
+    <div class="section">
+      <div class="ng-fm-row">
+        <p>
+          Remain Free tickets: {{ result.numberOfFreeTicket.toLocaleString() }}
+        </p>
+      </div>
+      <div class="ng-fm-row">
+        <p>LE tickets bought: {{ result.allLeTickets.toLocaleString() }}</p>
+      </div>
+      <div class="ng-fm-row">
+        <p>
+          Ticket in current round:
+          {{ result.numberOfTicketInRound.toLocaleString() }}
+        </p>
+      </div>
+      <br />
+      <div class="ng-fm-row">
+        <p>Flipcoin Win: {{ result.numberOfBettingWin.toLocaleString() }}</p>
+      </div>
+      <div class="ng-fm-row">
+        <p>Flipcoin Lose: {{ result.numberOfBettingLose.toLocaleString() }}</p>
+      </div>
+      <div class="ng-fm-row">
+        <p>Flipcoin X10: {{ result.times10BettingCount.toLocaleString() }}</p>
+      </div>
+      <div class="ng-fm-row">
+        <p>
+          Flipcoin Win Total Amount:
+          {{ result.winTotalAmount.toLocaleString() }}
+        </p>
+      </div>
+      <div class="ng-fm-row">
+        <p>
+          Flipcoin Lose Total Amount:
+          {{ result.loseTotalAmount.toLocaleString() }}
+        </p>
+      </div>
+      <div class="ng-fm-row">
+        <p>
+          Average betting amount:
+          {{ result.bettingAmountAverage.toLocaleString() }}
+        </p>
+      </div>
+      <br />
     </div>
-    <div class="ng-fm-row">
-      <p>Number of decorations: {{ result.decors.toLocaleString() }}</p>
-    </div>
-    <br />
-    <div class="ng-fm-row">
-      <p>Number of bidding slots: {{ result.bidding.toLocaleString() }}</p>
-    </div>
-    <div class="ng-fm-row">
-      <p>Number of ceiling bids: {{ result.highestBid.toLocaleString() }}</p>
-    </div>
-    <br />
-    
-    <div class="ng-fm-row">
-      <p>Legend plants: {{ result.legendPlants.toLocaleString() }}</p>
-    </div>
-    <div class="ng-fm-row">
-      <p>Epic plants: {{ result.epicPlants.toLocaleString() }}</p>
-    </div>
-    <div class="ng-fm-row">
-      <p>Rare plants: {{ result.rarePlants.toLocaleString() }}</p>
-    </div>
-    <div class="ng-fm-row">
-      <p>Common plants: {{ result.commonPlants.toLocaleString() }}</p>
-    </div>
-    <div class="ng-fm-row">
-      <p>Mother trees: {{ result.motherTrees.toLocaleString() }}</p>
-    </div>
-    <div class="ng-fm-row">
-      <p>Fused plants: {{ result.fused.toLocaleString() }}</p>
-    </div>
-    <div class="ng-fm-row">
-      <p>Omega plants: {{ result.omega.toLocaleString() }}</p>
-    </div>
-    <br />
-    <div class="ng-fm-row">
-      <p>
-        Remain Free tickets: {{ result.numberOfFreeTicket.toLocaleString() }}
-      </p>
-    </div>
-    <div class="ng-fm-row">
-      <p>LE tickets bought: {{ result.allLeTickets.toLocaleString() }}</p>
-    </div>
-    <div class="ng-fm-row">
-      <p>
-        Ticket in current round:
-        {{ result.numberOfTicketInRound.toLocaleString() }}
-      </p>
-    </div>
-    <br />
-    <div class="ng-fm-row">
-      <p>Flipcoin Win: {{ result.numberOfBettingWin.toLocaleString() }}</p>
-    </div>
-    <div class="ng-fm-row">
-      <p>Flipcoin Lose: {{ result.numberOfBettingLose.toLocaleString() }}</p>
-    </div>
-    <div class="ng-fm-row">
-      <p>Flipcoin X10: {{ result.times10BettingCount.toLocaleString() }}</p>
-    </div>
-    <div class="ng-fm-row">
-      <p>
-        Flipcoin Win Total Amount: {{ result.winTotalAmount.toLocaleString() }}
-      </p>
-    </div>
-    <div class="ng-fm-row">
-      <p>
-        Flipcoin Lose Total Amount:
-        {{ result.loseTotalAmount.toLocaleString() }}
-      </p>
-    </div>
-    <div class="ng-fm-row">
-      <p>
-        Average betting amount:
-        {{ result.bettingAmountAverage.toLocaleString() }}
-      </p>
-    </div>
-    <br />
-    <div class="ng-fm-row">
-      <p>Curent convert: {{ result.currentConvert.toLocaleString() }}</p>
-    </div>
-    <div class="ng-fm-row">
-      <p>
-        Current convert amount:
-        {{ result.currentConvertAmount.toLocaleString() }}
-      </p>
-    </div>
-    <div class="ng-fm-row">
-      <p>Checking record: {{ result.needCheckingConvert }}</p>
-    </div>
-    <br />
-    <div class="ng-fm-row">
-      <p>LE Dev pool: {{ result.leDevPool.toLocaleString() }}</p>
-    </div>
-    <div class="ng-fm-row">
-      <p>LE treasury pool: {{ result.leTreasuryPool.toLocaleString() }}</p>
+    <div class="section">
+      <div class="ng-fm-row">
+        <p>Curent convert: {{ result.currentConvert.toLocaleString() }}</p>
+      </div>
+      <div class="ng-fm-row">
+        <p>
+          Current convert amount:
+          {{ result.currentConvertAmount.toLocaleString() }}
+        </p>
+      </div>
+      <div class="ng-fm-row">
+        <p>Checking record: {{ result.needCheckingConvert }}</p>
+      </div>
+      <br />
+      <div class="ng-fm-row">
+        <p>LE Dev pool: {{ result.leDevPool.toLocaleString() }}</p>
+      </div>
+      <div class="ng-fm-row">
+        <p>LE treasury pool: {{ result.leTreasuryPool.toLocaleString() }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -122,3 +132,11 @@ function submit() {
   console.log(success)
 }
 </script>
+<style lang="scss" scoped>
+.counter-page {
+  // display: flex;
+  // > .section {
+  //   margin-left: 100px;
+  // }
+}
+</style>
